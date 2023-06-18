4e6a40783d51f9a19955bbe4840c08ac1922c180
shanmugapriya9194 <136685377+shanmugapriya9194@users.noreply.github.com>
2023-06-18 16:08:14 +0530

    Update streamlit_app.py
    arranging code to handle error

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,17 +20,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    #streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+  
 
 #request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 #streamlit.text(fruityvice_response.json())
 
 # creating normalized data out of json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # now showing the normalized data on screen as table
-streamlit.dataframe(fruityvice_normalized)
+
 streamlit.stop()
 
 #snowflake connector
